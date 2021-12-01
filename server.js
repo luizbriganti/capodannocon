404aca8f3e87d828c98d322ed7de0877e9a5d2b1
l.briganti <l.briganti@advisespa.it>
2021-12-01 12:55:35 +0100

    ljlkj

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const app = jsonServer.create();
-//const path = require('path');
+const path = require('path');
 //const express = require('express');
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults(
@@ -15,6 +15,8 @@ app.use(middlewares);
 app.use(jsonServer.rewriter({   
    '/api/*': '/$1'
 }))
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './build', 'index.html')))
 
 app.use(router)
 
