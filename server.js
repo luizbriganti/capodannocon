e806e6a5c49e410c5c6cf75e324894ddbd3e8b6e
l.briganti <l.briganti@advisespa.it>
2021-12-01 14:42:56 +0100

    lkjklj

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const jsonServer = require('json-server');
+/*const jsonServer = require('json-server');
 const app = jsonServer.create();
 const path = require('path');
 //const express = require('express');
@@ -22,4 +22,35 @@ app.use(router)
 
 app.listen(port, () => {
    console.log('Server is running')
+})*/
+
+const jsonServer = require('json-server')
+const express = require('express')
+const port = process.env.PORT || 8000;
+
+const server = express()
+
+const middlewares = jsonServer.defaults(
+   {
+      static: './build'
+   }
+);
+server.use(middlewares)
+
+server.use(jsonServer.rewriter({   
+   '/api/*': '/$1'
+}))
+
+server.use('/api', jsonServer.router('db.json'));
+
+server.get('/', (req, res) => {
+   res.send('root');
+})
+
+server.get('/login', (req, res) => {
+   res.send('login');
+})
+
+server.get('/verify/*', (req, res) => {
+   res.send('verify');
 })
