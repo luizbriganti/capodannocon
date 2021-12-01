c2a7a3d2ae3df1a0bb0e250cbcb528755f4a669a
l.briganti <l.briganti@advisespa.it>
2021-12-01 10:23:30 +0100

    ljlkjl

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+import express from 'express'
+import path from 'path'
+
 const jsonServer = require('json-server');
 const app = jsonServer.create();
 //const path = require('path');
 //const express = require('express');
-const router = jsonServer.router('./db.json');
+const dbRouter = jsonServer.router('./db.json');
+const router = jsonServer.router();
 const middlewares = jsonServer.defaults(
    {
       static: './build'
@@ -12,14 +16,21 @@ const port = process.env.PORT || 8000;
 
 app.use(middlewares);
 
-app.use(jsonServer.rewriter({
-   '/login': '/login',
-   '/verify/*': '/verify/$1',
+app.use(jsonServer.rewriter({   
    '/api/*': '/$1'
 }))
 
 app.use(router)
+app.use(dbRouter)
 
 app.listen(port, () => {
    console.log('Server is running')
+})
+
+const server = express() 
+
+server.set('etag', 'strong')
+
+server.use('/*', (req, res) => {
+   res.sendFile(path.join(__dirname, './build', 'index.html'))
 })
